f7fd4b7b4e54f4b1a182f2728512b8e6f6845071
heilxwotan1 <noobdima228@gmail.com>
2023-08-15 18:34:45 +0300

    1. created Book model 2. created BookRepository and BookRepositoryImpl 3. created BookService and BookServiceImpl 4. added dependencies to pom.xml

---
+++ src/main/java/com/bookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,20 @@
+package com.bookstore.repository.impl;
+
+import com.bookstore.model.Book;
+import com.bookstore.repository.BookRepository;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+
+    @Override
+    public Book save(Book book) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return null;
+    }
+}
