c6b8e33f0e702a58139229b48f0b0dc7c57e7e66
heilxwotan1 <noobdima228@gmail.com>
2023-08-16 13:23:48 +0300

    added implementation to BookRepositoryImpl and BookServiceImpl

--- src/main/java/com/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/BookServiceImpl.java
@@ -1,4 +1,22 @@
 package com.bookstore.service.impl;
 
-public class BookServiceImpl {
+import com.bookstore.model.Book;
+import com.bookstore.repository.BookRepository;
+import com.bookstore.service.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
 }
